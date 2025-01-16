16213ed87573841a1c5fabfdc84d76ce1a491ad5
Axel Frache <axelfrache@gmail.com>
2025-01-16 10:44:56 +0100

    Config de sécurité temporaire.

--- src/main/java/fr/umontpellier/bloomcycle/configuration/SecurityConfig.java
+++ src/main/java/fr/umontpellier/bloomcycle/configuration/SecurityConfig.java
@@ -2,8 +2,11 @@ package fr.umontpellier.bloomcycle.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
@@ -12,4 +15,13 @@ public class SecurityConfig {
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
+    // Temporary test configuration to disable CSRF and allow all requests
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
+        return http.build();
+    }
+
 }
--- src/main/java/fr/umontpellier/bloomcycle/controller/UserController.java
+++ src/main/java/fr/umontpellier/bloomcycle/controller/UserController.java
@@ -1,24 +1,35 @@
 package fr.umontpellier.bloomcycle.controller;
 
-import fr.umontpellier.bloomcycle.repository.UserRepository;
+import fr.umontpellier.bloomcycle.model.User;
 import fr.umontpellier.bloomcycle.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/api/users")
 public class UserController {
 
-    private final UserRepository userRepository;
-    private final PasswordEncoder passwordEncoder;
     private final UserService userService;
 
     @Autowired
-    public UserController(UserRepository userRepository, PasswordEncoder passwordEncoder, UserService userService) {
-        this.userRepository = userRepository;
-        this.passwordEncoder = passwordEncoder;
+    public UserController(UserService userService) {
         this.userService = userService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<User> registerUser(@RequestBody User user) {
+        User createdUser = userService.registerUser(user);
+        return ResponseEntity.ok(createdUser);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<User> loginUser(@RequestParam String email, @RequestParam String password) {
+        User authenticatedUser = userService.loginUser(email, password);
+        return ResponseEntity.ok(authenticatedUser);
     }
 }
