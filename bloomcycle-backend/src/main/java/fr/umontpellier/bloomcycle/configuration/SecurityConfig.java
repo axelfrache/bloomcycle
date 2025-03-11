370562fd281d02da64cffe6740fa8be314e0653f
Axel Frache <axelfrache@gmail.com>
2025-03-11 10:50:59 +0100

    Add CORS configuration to security filter chain.

--- bloomcycle-backend/src/main/java/fr/umontpellier/bloomcycle/configuration/SecurityConfig.java
+++ bloomcycle-backend/src/main/java/fr/umontpellier/bloomcycle/configuration/SecurityConfig.java
@@ -43,6 +43,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(AUTH_WHITELIST).permitAll()
