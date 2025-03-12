8a52996f83756f06e54e70ec38c2c17249021230
Axel Frache <axelfrache@gmail.com>
2025-03-12 13:16:53 +0100

    Update CORS configuration to restrict allowed origins.

--- bloomcycle-backend/src/main/java/fr/umontpellier/bloomcycle/configuration/SecurityConfig.java
+++ bloomcycle-backend/src/main/java/fr/umontpellier/bloomcycle/configuration/SecurityConfig.java
@@ -61,11 +61,11 @@ public class SecurityConfig {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("*"));
+        configuration.setAllowedOrigins(List.of("http://localhost:4200", "https://bloomcycle.microsoftgroups.org"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type", "Origin", "Accept", "X-Requested-With"));
         configuration.setExposedHeaders(List.of("Authorization"));
-        configuration.setAllowCredentials(false);
+        configuration.setAllowCredentials(true);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
